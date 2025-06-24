9840f7cabfa920657462b02e13c8d63ea94bf62a
WouterXillio <wouter.ubbink@xillio.com>
2025-06-24 16:58:50 +0200

    test2

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/frontend/",
   plugins: [react()],
-  assetsInclude: ["**/*.md"],
-});
+  assetsInclude: ['**/*.md'],
+  build: {
+    outDir: 'build',
+  },
+})
